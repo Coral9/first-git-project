1f7a80303cd9a1498bac390361bf4ccb80552b9d
katerina.trajanovska <katerina.trajanovska@symphony-solutions.eu>
2016-12-01 12:47:44 +0100

    Completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu(); //the mobileMenu object is a new instance of the MobileMenu class
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '100%');
+var stickyHeader = new StickyHeader();
