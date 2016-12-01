10c43f98621216473897a56153895868707773c5
katerina.trajanovska <katerina.trajanovska@symphony-solutions.eu>
2016-12-01 16:45:12 +0100

    Completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu(); //the mobileMenu object is a new instance of the MobileMenu class
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '100%');
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
